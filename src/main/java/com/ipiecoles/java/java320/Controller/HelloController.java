3364b36de27167a8c104ed598893a9f919d0ee67
remifoyard <rfpro8@gmail.com>
2018-02-07 10:01:59 +0100

    2018_02_07 10H01

--- src/main/java/com/ipiecoles/java/java320/Controller/HelloController.java
+++ src/main/java/com/ipiecoles/java/java320/Controller/HelloController.java
@@ -1,18 +1,62 @@
 package com.ipiecoles.java.java320.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.MediaPrintableArea;
+import java.awt.*;
 
 @RestController
 @RequestMapping("/sayHello")
 public class HelloController {
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "text/html")
-    public String sayHello(){
+    public String sayHello1(){
         String Response = "<html><head>";
         Response+="<title>Hello Servet</title>";
         Response+="</head><body>";
         Response+="<h1>Hello !</h1>";
+        Response+="</body></html>";
+        return Response;
+    }
+
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "text/html", params = {"nom","prenom"} )
+    public String sayHello2(@RequestParam("nom") String nom,@RequestParam("prenom") String prenom){
+        String Response = "<html><head>";
+        Response+="<title>Hello Servet</title>";
+        Response+="</head><body>";
+        Response+="<h1>Hello " + nom + " " + prenom + " !</h1>";
+        Response+="</body></html>";
+        return Response;
+    }
+
+    @RequestMapping(value = "/{name}/{firstname}", method = RequestMethod.GET, produces = "text/html" )
+    public String sayHello3(@PathVariable(value = "name") String name,@PathVariable(value = "firstname") String firstname){
+        String Response = "<html><head>";
+        Response+="<title>Hello Servet</title>";
+        Response+="</head><body>";
+        Response+="<h1>Hello " + name + " " + firstname + "  !</h1>";
+        Response+="</body></html>";
+        return Response;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = "text/html", consumes = "application/json" )
+    public String sayHello4(@RequestBody UserStats userStats){
+        String Response = "<html><head>";
+        Response+="<title>Hello Servet</title>";
+        Response+="</head><body>";
+        Response+="<h1>Hello " + userStats.getLastName() + " " + userStats.getFirstName() + " !</h1><h2>" + userStats.getTel() + "</h2><h3>" + userStats.getMail() + "</h3>";
+        Response+="</body></html>";
+        return Response;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String sayHello5(@RequestBody MultiValueMap<String,String> map){
+        String Response = "<html><head>";
+        Response+="<title>Hello Servet</title>";
+        Response+="</head><body>";
+        Response+="<h1>Hello " + map.getFirst("nom") + "</h3>";
         Response+="</body></html>";
         return Response;
     }
---
+++ src/main/java/com/ipiecoles/java/java320/Controller/UserStats.java
@@ -0,0 +1,40 @@
+package com.ipiecoles.java.java320.Controller;
+
+public class UserStats{
+    private String firstName;
+    private String lastName;
+    private String tel;
+    private String mail;
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
